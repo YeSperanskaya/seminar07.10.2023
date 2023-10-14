7a1b3aa1a7d00e2cd04a181cce2eb77c37f14a03
Evgeniya <speranskaya1991@mail.ru>
2023-10-14 13:39:43 +0500

    added task 57

---
+++ task55/Program.cs
@@ -0,0 +1,132 @@
+﻿// // Задача 55:
+// // Задайте двумерный массив. Напишите программу,
+// // которая заменяет строки на столбцы. В случае, если это
+// // невозможно, программа должна вывести сообщение для
+// // пользователя.
+
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+        
+//     }
+
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+    
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("|");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($" {matrix[i, j], 6}");
+//         }
+//         Console.WriteLine("    |");
+//     }
+   
+// }
+
+
+// void ReplaseRowColumt(int [,] matrix)
+// {
+//     int row = matrix.GetLength(0);
+//     int column = matrix.GetLength(1);
+//     if (row != column)
+//     {
+//         Console.WriteLine("no");
+//     }
+//     else
+//     {      
+//         for (int i = 0; i < matrix.GetLength(0)-1; i++)
+//         {
+//             for (int j = i + 1; j < matrix.GetLength(1); j++)
+//             {
+//                 int temp = matrix[i, j];
+//                 matrix[i, j] = matrix[j, i];
+//                 matrix[j, i] = temp;
+//             }
+//         }
+//     }
+// }
+
+// int[,] array2d = CreateMatrixRndInt(6, 5, -10, 10);
+// PrintMatrix(array2d);
+// ReplaseRowColumt(array2d);
+// Console.WriteLine();
+// PrintMatrix(array2d);
+
+
+
+int[,] CreatMatrixRandomInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0}\t", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+//     0 1 2 3         0 1 2 3
+
+// 0   1 2 3 4     0   1 1 1 1
+// 1   1 2 3 4     1   2 2 2 2
+// 2   1 2 3 4     2   3 3 3 3
+// 3   1 2 3 4     3   4 4 4 4
+// 1,0
+// 2,0
+// 2,1
+// 3,0
+// 3,1
+// 3,2
+
+void ReplaceRowsWithColomns(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+int[,] rndMatrix = CreatMatrixRandomInt(4, 4, 1, 9);
+PrintArray(rndMatrix);
+
+Console.WriteLine();
+if (rndMatrix.GetLength(0) != rndMatrix.GetLength(1))
+{
+    Console.WriteLine("Невозможно заменить строки на столюцы!");
+    return;
+}
+ReplaceRowsWithColomns(rndMatrix);
+PrintArray(rndMatrix);
---
+++ task57/Program.cs
@@ -0,0 +1,164 @@
+﻿// // Задача 57:
+// // Составить частотный словарь элементов
+// // двумерного массива. Частотный словарь содержит
+// // информацию о том, сколько раз встречается элемент
+// // входных данных.
+
+
+// int[,] CreatMatrixRandomInt(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new();
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write("{0}\t", matrix[i, j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] rndMatrix = CreatMatrixRandomInt(4, 4, 1, 9);
+// PrintArray(rndMatrix);
+
+// Console.WriteLine();
+
+// int[] MatrixInArray(int[,] matrix)
+// {
+//     int k = 0;
+//     int[] array = new int[matrix.Length];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; matrix.GetLength(1); j++)
+//         {
+//             array[k] = matrix[i, j];
+//             k++;
+//         }
+//     }
+//     return array;
+// } 
+
+// void PrintFrequencyDictionary(int[] array)
+// {
+//     int count = 1;
+//     int currentNum = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         currentNum = array[i];
+//         if (array[i] == currentNum)
+//         {
+//             count++;
+//         }
+//         else
+//         {
+//             Console.Write($"{currentNum}, -> {count}");
+//             currentNum = array[i];
+//             count = 1;
+//         }
+
+//     }
+    
+    
+//     int[,] matrix = new int[,];
+// }
+
+// int[,] matrix = new int[4, 4];
+// CreatMatrixRandomInt(matrix);
+// PrintArray(matrix)
+
+// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0,4}", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] PullMatrix(int[,] matrix)
+{
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 15);
+        }
+    }
+    return matrix;
+}
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int k = 0;
+    int[] array = new int[matrix.Length];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+    return array;
+}
+
+// 1 1 5 5 6 6 9 9
+void PrintFrequencyDictionary(int[] array)
+{
+    int count = 1;
+    int currentNum = array[0];
+    for(int i = 1; i<array.Length; i++)
+    {
+        if (array[i] == currentNum)
+        {
+            count++;
+        } else
+        {
+            Console.WriteLine($"{currentNum} -> {count}");
+            currentNum = array[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"{currentNum} -> {count}");
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{0,4}", array[i]);
+    }
+}
+
+int[,] matrix = new int[4, 4];
+PullMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] arr = MatrixToArray(matrix);
+Array.Sort(arr);
+PrintArray(arr);
+Console.WriteLine();
+PrintFrequencyDictionary(arr);
+
+
