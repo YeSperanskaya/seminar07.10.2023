4c7129fe7fa031a19c2fb1433e9746add7a7eabb
Evgeniya <speranskaya1991@mail.ru>
2023-10-15 13:38:32 +0500

    added task 69

---
+++ task65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void RangeNatureNumbers (int numA, int numB)
+{
+    if(numA == numB) 
+    {
+        Console.Write($"{numA}");        
+    }
+    else if (numA > numB)
+    {
+        Console.Write($"{numA} ");
+        RangeNatureNumbers(numA - 1, numB);
+    }
+    else 
+    {
+        Console.Write($"{numA} ");
+        RangeNatureNumbers(numA + 1, numB);
+    }
+}
+
+Console.WriteLine("Введите первое натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{numberM}, {numberN} -> ");
+RangeNatureNumbers(numberN, numberM);
+
+
+
+
---
+++ task69/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// int PowRecursive(int a, int b)
+// {
+// 	if(b == 0) return 1;
+// 	return a * PowRecursive(a, b - 1);
+//     //4%10 + 45%10 + 453%10 + 0 = 12
+// }
+
+// Console.WriteLine(PowRecursive(3, 5)); // 120
+
+
+
+int PowRecursive(int a, int b) // (3,5) (3,4) (3,3) (3,2) (3,1) (3,0)
+{
+    if(b == 0) return 1; //
+    return a * PowRecursive(a, b - 1); // (3,5) (3,4) (3,3) (3,2) (3,1)
+    // 3 * 3 * 3 * 3 * 3 * 1 = 243
+}
+
+System.Console.WriteLine("Input natural number A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input natural number A: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = PowRecursive(numberA, numberB);
+System.Console.WriteLine($"{numberA}^{numberB} -> {result}");
